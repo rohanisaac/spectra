1de738545488b87e68d3007f02342fab66f17d9c
Rohan Isaac <rohan_isaac@yahoo.com>
2016-02-22 17:05:36 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='spectra',
+      version='0.2',
+      description='Basic spectral analysis using lmfit',
+      author='Rohan Isaac',
+      author_email='rohan_isaac@yahoo.com',
+      url='https://github.com/rohanisaac/spectra',
+      packages=['spectra'],
+      install_requires=['numpy','lmfit'],
+      )
