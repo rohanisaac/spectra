541ecc2b189484841a257249180062241f1ee67d
Rohan Isaac <rohan_isaac@yahoo.com>
2018-05-23 16:15:47 -0400

    Split up nanometer to eV conversion depending of data shape

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -6,7 +6,7 @@ Doing this explicitly for clarity
 from .peaks import find_peaks, lorentzian, gaussian, voigt
 from .fitting import fit_data, fit_data_bg, output_results, batch_fit_single_peak, line_fit
 from .array_help import find_nearest, find_nearest_index, copy_range, copy_range_array, sort_by_list, find_nearest_tolerance, copy_range_yarray
-from .convert import wl2wn, wn2wl, rwn2wl, rwn2wn, absorption, nm2ev
+from .convert import wl2wn, wn2wl, rwn2wl, rwn2wn, absorption, nm2ev, nm2ev_xy, nm2ev_xyz
 from .file_io import assure_path_exists, write2col, getxy, clean_file, \
                     path, load_folder, quick_load_xy
 from .normalize import normalize, normalize_msc, normalize_pq, normalize_2pt
--- spectra/convert.py
+++ spectra/convert.py
@@ -33,7 +33,14 @@ def absorption(transmission):
     """
     return (2 - np.log10(transmission))
 
-def nm2ev(x, y):
+def nm2ev(x):
+    """
+    Convert nm to eV
+    """
+    c1 = sc.h * sc.c / ( sc.eV * sc.nano) # hc = 1240 eV nm
+    return c1/x
+
+def nm2ev_xy(x, y):
     """
     Properly convert data from wavelength on the x-scale from nm to eV
     
@@ -43,3 +50,26 @@ def nm2ev(x, y):
     x_c = c1 / x # E = hc/lambda in eV
     y_c = y / np.power(x_c, 2) # f(E) = f(lambda) hc/E^2
     return x_c, y_c
+
+def nm2ev_xyz(x, y, z):
+    """
+    Properly convert 2d data from nm to eV
+    
+    Parameters
+    ----------
+    
+    x: some axis
+    y: nm
+    z: intensity
+    
+    Returns
+    -------
+    x: same
+    y: eV
+    z: corrected intensity
+    """
+    print(x.shape, y.shape, z.shape)
+    c1 = sc.h * sc.c / ( sc.eV * sc.nano) # hc = 1240 eV nm
+    y_c = c1 / y # E = hc/lambda in eV
+    z_c = z.T / np.power(y_c, 2) # f(E) = f(lambda) hc/E^2
+    return x, y_c, z_c.T
